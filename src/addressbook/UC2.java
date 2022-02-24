5a9bda20367157a9dac4baaddda8b6f8ce86aec5
Shardulkumbhar <kumbharshardul384@gmail.com>
2022-02-24 11:57:15 +0530

    added uc2 problem by arraylist file

---
+++ src/addressbook/UC2.java
@@ -0,0 +1,62 @@
+package addressbook;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+class Addressbook {
+
+	void dataAdd() {
+
+		// scanner to take user input
+		try (Scanner scanner = new Scanner(System.in)) {
+			ArrayList<String> contacts = new ArrayList<String>();
+
+			System.out.print("Enter First Name: ");
+			String firstn = scanner.nextLine();
+			contacts.add(firstn);
+
+			System.out.print("Enter Last Name: ");
+			String lastn = scanner.nextLine();
+			contacts.add(lastn);
+
+			System.out.print("Enter Address: ");
+			String add = scanner.nextLine();
+			contacts.add(add);
+
+			System.out.print("Enter City: ");
+			String city = scanner.nextLine();
+			contacts.add(city);
+
+			System.out.print("Enter State: ");
+			String state = scanner.nextLine();
+			contacts.add(state);
+
+			System.out.print("Enter Zip Code: ");
+			String zip = scanner.nextLine();
+			contacts.add(zip);
+
+			System.out.print("Enter Phone No: ");
+			String phoneno = scanner.nextLine();
+			contacts.add(phoneno);
+
+			System.out.print("Enter Email: ");
+			String email = scanner.nextLine();
+			contacts.add(email);
+
+			// display or print a array list
+			System.out.println("Contact Added");
+			System.out.println(contacts);
+		}
+	}
+}
+
+public class UC2 {
+
+	public static void main(String[] args) {
+		System.out.println("PhoneBook");
+		// creating object
+		Addressbook person1 = new Addressbook();
+		// calling methord
+		person1.dataAdd();
+	}
+}
